f7f45ce1f64e588c7cb2a39e792efcd7bfa75d7b
sarvasvaKhare <f20190752p@pilani.bits-pilani.ac.in>
2021-03-23 02:27:56 +0530

    done

--- schedule.js
+++ schedule.js
@@ -16,30 +16,33 @@ schedule.createSchedule = async function (data,token1,token2)
                                 },
     });   
      await scheduledNotification.save();
+     const timeToSent = data.time.split(":");  
      var hours = timeToSent[0];
-     var minutes = timeToSent[1];  
-     if(hours>=5){
-         if(minutes>=30){
-             hours=hours-5;
-             minutes=minutes-30;
-         }else{
-             if(hours==5){
-                 hours=23
-                 minutes=minutes+30
-             }else{
-                 hours=hours-6
-                 minutes=minutes+30
-             }
-         }
-     }else{
-        if(minutes>=30){
-            hours=hours+24-5;
-            minutes=minutes-30;
+     var minutes = timeToSent[1];
+     var hours = timeToSent[0];
+var minutes = timeToSent[1];  
+if(hours>=5){
+    if(minutes>=30){
+        hours=hours-5;
+        minutes=minutes-30;
+    }else{
+        if(hours==5){
+            hours=23
+            minutes=minutes+30
         }else{
-            hours=hours+24-6;
-            minutes=minutes+30;
+            hours=hours-6
+            minutes=minutes+30
         }
-     }  
+    }
+}else{
+   if(minutes>=30){
+       hours=hours+24-5;
+       minutes=minutes-30;
+   }else{
+       hours=hours+24-6;
+       minutes=minutes+30;
+   }
+}   
      const scheduleId = scheduledNotification._id.toString();    
      const scheduleTimeout = `${minutes} ${hours} * * *`;  
      const tokens=[token1,token2]  
