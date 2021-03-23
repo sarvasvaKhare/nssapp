f63ea4a86f97895ea8cdaa75e2ad3eae2cc078e9
sarvasvaKhare <f20190752p@pilani.bits-pilani.ac.in>
2021-03-23 19:46:55 +0530

    don3

--- models/user.js
+++ models/user.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
     name:   String,
     PHOTO: String,
     dept: String,
+    m_number: Number,
     designation: String,
     ACCESSLEVEL: String,
     QRCODE: String,
