e752e930e1f9f7de21387d91f7269d8cf4f84d69
sarvasvaKhare <f20190752p@pilani.bits-pilani.ac.in>
2021-03-23 20:44:16 +0530

    so much to check

--- models/ScheduledNotification.js
+++ models/ScheduledNotification.js
@@ -3,7 +3,7 @@ const schema = new mongoose.Schema(
     {    
         time: {type: String},
         days: {type: []},
-        notification: {},
+        data: {},
         expire_at: {type: Date, default: Date.now, expires: 86400} 
     });
 const ScheduledNotification = mongoose.model("scheduledNotification", schema);
